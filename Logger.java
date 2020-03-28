0c9038718ce999e00a5323c3dc9dbe5631829394
vinayak <vinayak.chaturvedi96@gmail.com>
2020-03-29 02:22:38 +0530

    Commit Refactored code

--- Logger.java
+++ Logger.java
@@ -1,67 +1,59 @@
-package DesktopScurityTool;
-import java.awt.*;
-import java.awt.event.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.Robot;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import javax.imageio.ImageIO;
-public class Logger extends JFrame implements ActionListener,Runnable
-{
-   JButton b1,b2;
-   JPanel p1;
-   Thread t1;
-   int i=0;
-   public Logger()
-   {
-       setSize(200,200);
-       setVisible(true);
-       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-       b1=new JButton("Start");
-       b2=new JButton("Stop");
-       p1=new JPanel();
-       p1.add(b1);
-       p1.add(b2);
-       getContentPane().add(p1);
-       b1.addActionListener(this);
-       b2.addActionListener(this);
-       setSize(200,200);
-   }
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource()==b1)
-        {
-            t1=new Thread(this);
+
+public class Logger extends JFrame implements ActionListener, Runnable {
+    JButton b1, b2;
+    JPanel p1;
+    Thread t1;
+    int i = 0;
+
+    public Logger() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        b1 = new JButton("Start");
+        b2 = new JButton("Stop");
+        p1 = new JPanel();
+        p1.add(b1);
+        p1.add(b2);
+        getContentPane().add(p1);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        setSize(200, 200);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            t1 = new Thread(this);
             t1.start();
-        }
-        else
-        {
+        } else {
             t1.stop();
             System.exit(0);
         }
     }
-    public void run()
-    {
-        for(;;)
-        {
-            try
-            {
-                Robot r=new Robot();
-                BufferedImage img=r.createScreenCapture(new java.awt.Rectangle(getToolkit().getScreenSize()));
-                ImageIO.write(img,"jpg",new File("c:\\pic\\"+i+".jpg"));
+
+    public void run() {
+        for (; ; ) {
+            try {
+                Robot r = new Robot();
+                BufferedImage img = r.createScreenCapture(new java.awt.Rectangle(getToolkit().getScreenSize()));
+                ImageIO.write(img, "jpg", new File("c:\\pic\\" + i + ".jpg"));
                 i++;
                 t1.sleep(500);
-                
-            }
-            catch(Exception ex)
-            {
-                
+
+            } catch (Exception ex) {
+
             }
         }
     }
-    public static void main(String args[])
-    {
-       new Logger(); 
+
+    public static void main(String args[]) {
+        new Logger();
     }
-    
+
 }
